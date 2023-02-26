ff2bb2d3268100d78b80034c10d3939d7667392a
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-26 13:10:36 -0500

    implementado caso holamundo

--- src/app/holamundo/holamundo.component.ts
+++ src/app/holamundo/holamundo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-holamundo',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HolamundoComponent {
 
+  saludo: string = '';
+  error: any = '';
+
+  constructor(private http: HttpClient) { }
+
+  hola() {
+    this.saludo = '';
+    this.error = '';
+    this.http.get<any>('http://localhost:3000/hola')
+    .subscribe(data => {
+      this.saludo = data.saludo;
+    }, error => {
+      this.error = error.message;
+    });
+
+  }
 }
