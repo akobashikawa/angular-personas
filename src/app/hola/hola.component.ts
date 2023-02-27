6366734f48b3b24f5b36a54977454f4749c161d2
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-26 22:01:07 -0500

    implementado ejemplo hola

--- src/app/hola/hola.component.ts
+++ src/app/hola/hola.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from "../data.service";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-hola',
   templateUrl: './hola.component.html',
   styleUrls: ['./hola.component.css']
 })
-export class HolaComponent {
+export class HolaComponent implements OnInit, OnDestroy {
 
+  backendBaseUrl: string = '';
+  subscription: Subscription = new Subscription;
+
+  nombre: string = '';
+  saludo: string = '';
+  error: any = '';
+
+  constructor(private http: HttpClient, private data: DataService) { }
+
+  ngOnInit() {
+    this.subscription = this.data.backendBaseUrl.subscribe(backendBaseUrl => this.backendBaseUrl = backendBaseUrl)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  hola() {
+    this.saludo = '';
+    this.error = '';
+    const body = {
+      nombre: this.nombre
+    };
+    this.http.post<any>(this.backendBaseUrl + '/hola', body)
+    .subscribe(data => {
+      this.saludo = data.saludo;
+    }, error => {
+      this.error = error.message;
+    });
+
+  }
 }
